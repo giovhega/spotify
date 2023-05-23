25799f108cf33974b033fe49d8c34e1fd159e35b
Giovanny <gio19gar@gmail.com>
2023-05-22 23:10:51 -0500

    se crean componentes, directivas y pipes

---
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -0,0 +1,39 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MultimediaService } from '@shared/services/multimedia.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-media-player',
+  templateUrl: './media-player.component.html',
+  styleUrls: ['./media-player.component.scss']
+})
+export class MediaPlayerComponent {
+  @ViewChild('progressBar') progressBar: ElementRef = new ElementRef('')
+  listObservers$: Array<Subscription> = []
+  state: string = 'paused'
+  constructor(public multimediaService: MultimediaService) { }
+
+  ngOnInit(): void {
+
+    const observer1$ = this.multimediaService.playerStatus$
+      .subscribe(status => this.state = status)
+    this.listObservers$ = [observer1$]
+  }
+
+  ngOnDestroy(): void {
+    this.listObservers$.forEach(u => u.unsubscribe())
+    console.log('🔴🔴🔴🔴🔴🔴🔴 BOOM!');
+  }
+
+
+  handlePosition(event: MouseEvent): void {
+    const elNative: HTMLElement = this.progressBar.nativeElement
+    const { clientX } = event
+    const { x, width } = elNative.getBoundingClientRect()
+    const clickX = clientX - x //TODO: 1050 - x
+    const percentageFromX = (clickX * 100) / width
+    console.log(`Click(x): ${percentageFromX}`);
+    this.multimediaService.seekAudio(percentageFromX)
+
+  }
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { MediaPlayerComponent } from './components/media-player/media-player.component';
+import { HeaderUserComponent } from './components/header-user/header-user.component';
+import { RouterModule } from '@angular/router';
+import { CardPlayerComponent } from './components/card-player/card-player.component';
+import { PlayListBodyComponent } from './components/play-list-body/play-list-body.component';
+import { PlayListHeaderComponent } from './components/play-list-header/play-list-header.component';
+import { SectionGenericComponent } from './components/section-generic/section-generic.component';
+import { OrderListPipe } from './pipes/order-list.pipe';
+import { ImgBrokenDirective } from './directives/img-broken.directive';
+
+
+
+@NgModule({
+  declarations: [
+    SideBarComponent,
+    MediaPlayerComponent,
+    HeaderUserComponent,
+    CardPlayerComponent,
+    PlayListBodyComponent,
+    PlayListHeaderComponent,
+    SectionGenericComponent,
+    OrderListPipe,
+    ImgBrokenDirective
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    SideBarComponent,
+    MediaPlayerComponent,
+    HeaderUserComponent,
+    CardPlayerComponent,
+    PlayListBodyComponent,
+    PlayListHeaderComponent,
+    SectionGenericComponent,
+    OrderListPipe,
+    ImgBrokenDirective
+
+  ]
+})
+export class SharedModule { }
